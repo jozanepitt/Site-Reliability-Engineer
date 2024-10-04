8ac936fd7a338f40a3de133cbf812a55a1fdde95
jozanepitt <69273769+jozanepitt@users.noreply.github.com>
2024-10-04 12:33:53 +0200

    Update main.tf

--- srn/terraform/main.tf
+++ srn/terraform/main.tf
@@ -1,1 +1,26 @@
+provider "aws" {
+  region = "us-west-2"
+}
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_eks_cluster" "main" {
+  name     = "my-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.main.id]
+  }
+}
+
+resource "aws_db_instance" "main" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "bar"
+  parameter_group_name = "default.mysql5.7"
+}
